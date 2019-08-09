f2c93e68e0b06c9c4d1e8dce060e5f5815009305
VictorGil <victorgilsanz@gmail.com>
2019-08-09 16:09:14 +0200

    Fixing bug, adding BigDecimal amount

--- src/main/java/net/devaction/entity/TransferEntity.java
+++ src/main/java/net/devaction/entity/TransferEntity.java
@@ -1,5 +1,7 @@
 package net.devaction.entity;
 
+import java.math.BigDecimal;
+
 /**
  * @author Víctor Gil
  *
@@ -9,33 +11,30 @@ public class TransferEntity{
 
     private String id;
     private String accountId;
-    private String amountUnscaled;
-    private String amountScale;
-    
+    private BigDecimal amount;    
 
-    public TransferEntity(String id, String accountId, String amountUnscaled, String amountScale){
+    @Override
+    public String toString(){
+        return "TransferEntity [id=" + id + ", accountId=" + accountId + ", amount=" + amount + "]";
+    }
+
+    public TransferEntity(String id, String accountId, BigDecimal amount){
         this.id = id;
         this.accountId = accountId;
-        this.amountUnscaled = amountUnscaled;
-        this.amountScale = amountScale;
+        this.amount = amount;
     }
     
     public TransferEntity(){}
-    
-    @Override
-    public String toString(){
-        return "TransferEntity [id=" + id + ", accountId=" + accountId + ", amountUnscaled=" + amountUnscaled + ", amountScale="
-                + amountScale + "]";
-    }
 
     @Override
     public int hashCode(){
         final int prime = 31;
         int result = 1;
+        
         result = prime * result + ((accountId == null) ? 0 : accountId.hashCode());
-        result = prime * result + ((amountScale == null) ? 0 : amountScale.hashCode());
-        result = prime * result + ((amountUnscaled == null) ? 0 : amountUnscaled.hashCode());
+        result = prime * result + ((amount == null) ? 0 : amount.hashCode());
         result = prime * result + ((id == null) ? 0 : id.hashCode());
+        
         return result;
     }
 
@@ -43,66 +42,60 @@ public class TransferEntity{
     public boolean equals(Object obj){
         if (this == obj)
             return true;
+        
         if (obj == null)
             return false;
+        
         if (getClass() != obj.getClass())
             return false;
+        
         TransferEntity other = (TransferEntity) obj;
         
-        // The id value should be unique, so it makes sense 
-        // that it is the first field to be checked
+        // The id must be unique, so it makes sense to be the first instance field
+        // to be checked
         if (id == null){
             if (other.id != null)
                 return false;
         } else if (!id.equals(other.id))
             return false;
-        if (amountScale == null){
-            if (other.amountScale != null)
+        
+        if (amount == null){
+            if (other.amount != null)
                 return false;
-        } else if (!amountScale.equals(other.amountScale))
+        } else if (!amount.equals(other.amount))
             return false;
-        if (amountUnscaled == null){
-            if (other.amountUnscaled != null)
-                return false;
-        } else if (!amountUnscaled.equals(other.amountUnscaled))
-            return false;
+        
         if (accountId == null){
             if (other.accountId != null)
                 return false;
         } else if (!accountId.equals(other.accountId))
             return false;
+        
         return true;
     }
 
     public String getId(){
         return id;
     }
-    
+
     public void setId(String id){
         this.id = id;
     }
-    
+
     public String getAccountId(){
         return accountId;
     }
-    
+
     public void setAccountId(String accountId){
         this.accountId = accountId;
     }
-    
-    public String getAmountUnscaled(){
-        return amountUnscaled;
+
+    public BigDecimal getAmount(){
+        return amount;
     }
-    
-    public void setAmountUnscaled(String amountUnscaled){
-        this.amountUnscaled = amountUnscaled;
-    }
-    
-    public String getAmountScale(){
-        return amountScale;
-    }
-    
-    public void setAmountScale(String amountScale){
-        this.amountScale = amountScale;
+
+    public void setAmount(BigDecimal amount){
+        this.amount = amount;
     }    
 }
+
