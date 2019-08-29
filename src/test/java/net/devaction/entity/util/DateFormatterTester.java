fbc0e419d37b5d6ce8ad6daefc78241041375f8f
VictorGil <victorgilsanz@gmail.com>
2019-08-30 01:20:35 +0200

    Added more code to DateFormatterTester

--- src/test/java/net/devaction/entity/util/DateFormatterTester.java
+++ src/test/java/net/devaction/entity/util/DateFormatterTester.java
@@ -20,5 +20,17 @@ public class DateFormatterTester{
         String dateTime = DateFormatter.getDateTimeString(millis);
         
         log.info("Current date and time: {} ({})", dateTime, millis);
+        
+        long maxJSinteger = (long) Math.pow(2, 53) - 1;
+        log.info("Maximum integer value in JavaScript: {} --> "
+                + "corresponding date: {}", 
+                maxJSinteger,
+                DateFormatter.getDateTimeString(maxJSinteger));
+        
+        long maxJavaInteger = Long.MAX_VALUE;
+        log.info("Maximum integer value in Java: {} --> "
+                + "corresponding date: {}", 
+                maxJavaInteger,
+                DateFormatter.getDateTimeString(maxJavaInteger));        
     }
 }
